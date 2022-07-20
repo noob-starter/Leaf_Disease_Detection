b43104950e35fc9f2f429e3620d8f449567b7323
noob-starter <pratikyawalkar5263@gmail.com>
2022-07-20 12:10:38 +0530

    first commit

---
+++ TrainingLeaf.py
@@ -0,0 +1,75 @@
+from keras.models import Sequential
+# initialize nn
+
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense
+
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+
+# basic cnn
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(128, 128, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(96, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(5, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(rescale=None,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('Dataset\\train',
+                                                 target_size=(128, 128),
+                                                 batch_size=10,
+                                                 class_mode='categorical')
+# print(test_datagen)
+labels = (training_set.class_indices)
+# print(labels)
+
+val_set = test_datagen.flow_from_directory('Dataset\\val',
+                                           target_size=(128, 128),
+                                           batch_size=10,
+                                           class_mode='categorical')
+
+labels2 = (val_set.class_indices)
+# print(labels2)
+
+model.fit_generator(training_set,
+                    steps_per_epoch=15,
+                    epochs=5,
+                    validation_data=val_set,
+                    validation_steps=5)
+
+model_json = model.to_json()
+
+with open("model1.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model1.h5")
+print("Saved model to disk")
